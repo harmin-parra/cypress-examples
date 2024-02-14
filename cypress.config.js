142be033dfb55cdba1fea9739ffa8158524a3c5d
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-14 22:29:22 +0100

    Add reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  //reporter: 'mochawesome',
+  reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    reportDir: "cypress/report/",
+    //reportFilename: "index.html",
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
